fede62a1767d5d9328a7515dfd7de264a41fb35f
Evgeniy Terekhin <evgeniy@tvbeat.com>
2022-12-06 08:25:12 +0200

    04

---
+++ bin/04/p1.rs
@@ -0,0 +1,25 @@
+use std::ops::RangeInclusive;
+
+static INPUT: &str = include_str!("input");
+
+fn parse_range(s: &str) -> (usize, usize) {
+    let (from, to);
+    text_io::scan!(s.bytes() => "{}-{}", from, to);
+
+    (from, to)
+}
+
+fn main() {
+    let result = INPUT
+        .lines()
+        .map(|line| {
+            let ranges = line.split(",").collect::<Vec<_>>();
+            let (lrange, rrange) = (parse_range(ranges[0]), parse_range(ranges[1]));
+
+            (lrange, rrange)
+        })
+        .filter(|((lx, ly), (rx, ry))| (((lx <= rx) && (ly >= ry)) || ((rx <= lx) && (ry >= ly))))
+        .count();
+
+    println!("{result}");
+}
