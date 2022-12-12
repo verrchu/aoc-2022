5b1bb5249cf57bb809a9c2c36d71659012236ebc
Evgeniy Terekhin <evgeniy@tvbeat.com>
2022-12-12 15:56:30 +0200

    12

---
+++ bin/12/p2.rs
@@ -0,0 +1,86 @@
+static INPUT: &str = include_str!("input");
+
+const START: (usize, usize) = (20, 0);
+const TARGET: (usize, usize) = (20, 43);
+
+fn main() {
+    let mut grid = Vec::<Vec<u8>>::new();
+    for line in INPUT.lines() {
+        grid.push(line.chars().map(|c| c as u8).collect());
+    }
+
+    let mut min_len = usize::MAX;
+
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            if grid[row][col] == 'a' as u8 {
+                if let Some(len) = path_length(&grid, (row, col)) {
+                    if len < min_len {
+                        min_len = len;
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{min_len}");
+}
+
+fn path_length(grid: &Vec<Vec<u8>>, (row, col): (usize, usize)) -> Option<usize> {
+    let mut progress = vec![vec![None::<usize>; grid[0].len()]; grid.len()];
+    progress[row][col] = Some(0);
+
+    let mut queue = vec![(row, col)];
+
+    loop {
+        if queue.is_empty() {
+            break;
+        }
+
+        let cells = queue.drain(..).collect::<Vec<_>>();
+        for cell in cells {
+            let neighbours = get_neighbours(&grid, cell);
+            let c = grid[cell.0][cell.1];
+            let s = progress[cell.0][cell.1].unwrap() + 1;
+
+            for n in neighbours {
+                let nc = grid[n.0][n.1];
+
+                if nc <= c + 1 {
+                    if let Some(ns) = progress[n.0][n.1] {
+                        if ns > s {
+                            progress[n.0][n.1] = Some(s);
+
+                            queue.push(n);
+                        }
+                    } else {
+                        progress[n.0][n.1] = Some(s);
+
+                        queue.push(n);
+                    }
+                }
+            }
+        }
+    }
+
+    progress[TARGET.0][TARGET.1]
+}
+
+fn get_neighbours(grid: &Vec<Vec<u8>>, (row, col): (usize, usize)) -> Vec<(usize, usize)> {
+    let mut neighbours = vec![];
+
+    if row > 0 {
+        neighbours.push((row - 1, col));
+    }
+    if col > 0 {
+        neighbours.push((row, col - 1));
+    }
+    if col < (grid[0].len() - 1) {
+        neighbours.push((row, col + 1));
+    }
+    if row < (grid.len() - 1) {
+        neighbours.push((row + 1, col));
+    }
+
+    neighbours
+}
